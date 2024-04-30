c6776e329630dbb5df0726ce5e6e620d2b1ea332
saifullah199 <mdkhalidsaifullah199@gmail.com>
2024-04-30 23:20:00 +0600

    vercel solved

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    
 
     const itemCollection = client.db('itemDB').collection('item')
     const subCategoryCollection = client.db('itemDB').collection('subcategory')
